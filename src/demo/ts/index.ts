a9da429be48b4f32fb48d711f509d1a89dd8ca52
Christian Stussak <stussak@mfo.de>
2022-06-16 14:06:54 +0200

    Add CSS based pan and zoom demo

--- src/demo/ts/index.ts
+++ src/demo/ts/index.ts
@@ -1,16 +1,40 @@
 import ready from 'document-ready';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import * as lib from '../../library/ts/index';
+function zoom(zoomFactor: number, duration = 10) {
+  const wrapper = document.querySelector('div.wrapper') as HTMLDivElement;
+  wrapper.style.setProperty('--zoom-factor', `${zoomFactor}`);
+  wrapper.style.setProperty('--transition-duration', `${duration}s`);
+}
 
-// eslint-disable-next-line @typescript-eslint/no-empty-function
-async function mainAsync() {}
+function pan(panTarget: { x: number; y: number }, duration = 10) {
+  const wrapper = document.querySelector('div.wrapper') as HTMLDivElement;
+  wrapper.style.setProperty('--pan-target-x', `${panTarget.x}`);
+  wrapper.style.setProperty('--pan-target-y', `${panTarget.y}`);
+  wrapper.style.setProperty('--transition-duration', `${duration}s`);
+}
 
-function main() {
-  mainAsync().then(
+function zoomPan(
+  zoomFactor: number,
+  panTarget: { x: number; y: number },
+  duration = 10,
+) {
+  zoom(zoomFactor, duration);
+  pan(panTarget, duration);
+}
+
+const viewer = {
+  zoom,
+  pan,
+  zoomPan,
+};
+
+Object.assign(window, { viewer });
+
+async function main() {}
+
+ready(() => {
+  main().then(
     () => {},
     () => {},
   );
-}
-
-ready(main);
+});
