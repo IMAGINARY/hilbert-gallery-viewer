c30c170c88598c1649338441e5fb1aee4591220d
Christian Stussak <stussak@mfo.de>
2022-06-22 16:21:39 +0200

    Reimplement demo

--- src/demo/ts/index.ts
+++ src/demo/ts/index.ts
@@ -1,56 +1,7 @@
 import ready from 'document-ready';
 
+import '../../library/ts/hilbert-gallery-viewer';
 import { HilbertGalleryViewer } from '../../library/ts/hilbert-gallery-viewer';
-
-function zoom(zoomFactor: number, duration = 10) {
-  const wrapper = document.querySelector('div.wrapper') as HTMLDivElement;
-  wrapper.style.setProperty('--zoom-factor', `${zoomFactor}`);
-  wrapper.style.setProperty('--transition-ol-duration', `${duration}s`);
-}
-
-function pan(panTarget: { x: number; y: number }, duration = 10) {
-  const wrapper = document.querySelector('div.wrapper') as HTMLDivElement;
-  wrapper.style.setProperty('--pan-target-x', `${panTarget.x}`);
-  wrapper.style.setProperty('--pan-target-y', `${panTarget.y}`);
-  wrapper.style.setProperty('--transition-ol-duration', `${duration}s`);
-}
-
-function zoomPan(
-  zoomFactor: number,
-  panTarget: { x: number; y: number },
-  duration = 10,
-) {
-  zoom(zoomFactor, duration);
-  pan(panTarget, duration);
-}
-
-const viewer = {
-  zoom,
-  pan,
-  zoomPan,
-};
-
-Object.assign(window, { viewer });
-
-function createActionButtons() {
-  const buttonActions = [
-    () => viewer.zoom(3),
-    () => viewer.pan({ x: 0.3, y: 0.7 }),
-    () => viewer.zoomPan(2, { x: 0.75, y: 0.25 }),
-  ];
-  const form = document.querySelector('form[id=actions]') as HTMLFormElement;
-  buttonActions.forEach((buttonAction) => {
-    const button = document.createElement('input');
-    button.type = 'button';
-    button.value = buttonAction
-      .toString()
-      .split(/(\r\n|\n|\r)/)
-      .map((s) => s.trim())
-      .join(' ');
-    button.onclick = buttonAction;
-    form.appendChild(button);
-  });
-}
 
 function randomKittenUrl() {
   return `https://placekitten.com/${300 + Math.floor(Math.random() * 100)}/${
@@ -75,20 +26,66 @@ function shuffle<T>(a: T[]): T[] {
   return result;
 }
 
-async function main() {
-  await new Promise<void>(ready);
-  createActionButtons();
+type Content = {
+  mimetype: string;
+  url: string;
+};
 
-  console.log(HilbertGalleryViewer);
+function randomFit() {
+  return ['contain', 'cover'][Math.floor(Math.random() * 2)];
+}
 
-  const hGViewer = document.querySelector(
-    'hilbert-gallery-viewer',
-  ) as HilbertGalleryViewer;
+function randomTransition() {
+  const types = [
+    () => ({ type: 'none', options: {} }),
+    () => ({
+      type: 'fade',
+      options: { duration: 1 + Math.random() * 5, color: 'black' },
+    }),
+    () => ({
+      type: 'cross-fade',
+      options: { duration: 1 + Math.random() * 5 },
+    }),
+  ];
+  return types[Math.floor(Math.random() * types.length)]();
+}
 
-  const images = Array.from({ length: 2 }, () => ({
+function randomAnimation() {
+  const types = [
+    () => ({ type: 'none', options: {} }),
+    () => {
+      const x = 0.25 + Math.random() * 0.5;
+      const y = 0.25 + Math.random() * 0.5;
+      const minDistance = Math.min(x, 1 - x, y, 1 - y);
+      const scale = 1 / minDistance;
+      return {
+        type: 'pan-zoom',
+        options: { duration: 1 + Math.random() * 30, to: { x, y, scale } },
+      };
+    },
+  ];
+  return types[Math.floor(Math.random() * types.length)]();
+}
+
+function randomShowCommand(content: Content) {
+  return {
+    action: 'show',
+    args: {
+      ...content,
+      fit: randomFit(),
+      transition: randomTransition(),
+      animation: randomAnimation(),
+    },
+  };
+}
+
+function createPlaylist() {
+  const images = Array.from({ length: 5 }, () => ({
     mimetype: 'image/jpeg',
     url: randomKittenUrl(),
   }));
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const otherPngImages = [
     new URL('../img/red.png', import.meta.url).href,
     new URL('../img/green.png', import.meta.url).href,
@@ -100,51 +97,54 @@ async function main() {
       url: 'http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4',
     },
   ];
-  const content = [...images, ...otherPngImages, ...videos];
+  const content = shuffle([...images, ...videos]);
 
-  await hGViewer.execute('preload', content);
+  const preloadCommand = {
+    action: 'preload',
+    args: content.map((c) => ({ ...c })),
+  };
 
-  const shuffledContent = shuffle(content);
-  const animationDuration = 5;
-  const transitionDuration = 3;
-  const delay = 3;
-  await hGViewer.execute('show', {
-    ...shuffledContent[shuffledContent.length - 1],
-    transition: {
-      type: 'none',
-      options: {},
-    },
+  const showCommands = content.map((c) => randomShowCommand({ ...c }));
+
+  return {
+    oneShot: [preloadCommand],
+    loop: showCommands,
+  };
+}
+
+async function main() {
+  await new Promise<void>(ready);
+
+  const hGViewer = document.querySelector(
+    'hilbert-gallery-viewer',
+  ) as HilbertGalleryViewer;
+
+  const playlist = createPlaylist();
+  console.log(JSON.stringify(playlist, null, 2), playlist);
+
+  playlist.oneShot.forEach(({ action, args }) => {
+    // eslint-disable-next-line no-console
+    hGViewer.execute(action, args).catch((e) => console.error(e));
   });
-  await sleep(1);
+
   for (
     let i = 0;
-    i < shuffledContent.length;
-    i = (i + 1) % shuffledContent.length
+    i < playlist.loop.length;
+    i = (i + 1) % playlist.loop.length
   ) {
-    const x = 0.25 + Math.random() * 0.5;
-    const y = 0.25 + Math.random() * 0.5;
-    const minDistance = Math.min(x, 1 - x, y, 1 - y);
-    const scale = 1 / minDistance;
-    // eslint-disable-next-line no-await-in-loop
-    await hGViewer.execute('show', {
-      ...content[i],
-      fit: 'cover',
-      transition: {
-        type: 'fade',
-        options: {
-          duration: transitionDuration,
-        },
-      },
-      animation: {
-        type: 'pan-zoom',
-        options: {
-          duration: animationDuration,
-          to: { x, y, scale },
-        },
-      },
-    });
-    // eslint-disable-next-line no-await-in-loop
-    await sleep(animationDuration + delay);
+    const { action, args } = playlist.loop[i];
+    // eslint-disable-next-line no-console,no-await-in-loop
+    await hGViewer.execute(action, args).catch((e) => console.error(e));
+
+    if (action === 'show') {
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      const animationDuration = (args?.animation?.duration as number) ?? 0;
+      const delay = animationDuration > 5 ? 1 : 1 + Math.random() * 5;
+
+      // eslint-disable-next-line no-await-in-loop
+      await sleep(animationDuration + delay);
+    }
   }
 }
 
