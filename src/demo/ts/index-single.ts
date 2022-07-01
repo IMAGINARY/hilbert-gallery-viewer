4aa31ca55b0acb23bfc11fe1dfafa964b67ed4d7
Christian Stussak <stussak@mfo.de>
2022-07-01 15:21:37 +0200

    Add index-single.html that executes a single predefined action
    This is helpful during development to execute the same single action
    after every reload.

---
+++ src/demo/ts/index-single.ts
@@ -0,0 +1,52 @@
+/* eslint-disable no-console */
+import ready from 'document-ready';
+
+import '../../library/ts/hilbert-gallery-viewer';
+import { HilbertGalleryViewer } from '../../library/ts/hilbert-gallery-viewer';
+
+async function main() {
+  await new Promise<void>(ready);
+
+  const hGViewer = document.querySelector(
+    'hilbert-gallery-viewer',
+  ) as HilbertGalleryViewer;
+
+  const action = 'show';
+  const args = {
+    mimetype: 'image/jpeg',
+    url: 'https://placekitten.com/349/326',
+    fit: 'cover',
+    color: 'black',
+    transition: {
+      type: 'none',
+      options: {},
+    },
+    animation: {
+      type: 'pan-zoom',
+      options: {
+        duration: 10,
+        to: {
+          x: 0.25,
+          y: 0.25,
+          scale: 2,
+        },
+      },
+    },
+  };
+  const executeDefault = () => hGViewer.execute(action, args);
+  Object.assign(window, { viewer: hGViewer, executeDefault });
+  console.info('Call executeDefault() to execute the default action:', {
+    action,
+    args,
+  });
+  console.info(
+    'Call viewer.execute(action, args) to execute your own actions.',
+  );
+  await executeDefault();
+}
+
+function uncaughtErrorHandler(error: Error) {
+  console.error('Uncaught error', error);
+}
+
+main().catch(uncaughtErrorHandler);
